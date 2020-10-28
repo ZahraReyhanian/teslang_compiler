741ecaba38cff934348b94107996db5d6cace2b7
ZahraReyhanian <reyhanyanz@gmail.com>
2020-10-28 19:48:41 +0330

    add some method for find tokens

--- file_ex.cpp
+++ file_ex.cpp
@@ -9,12 +9,10 @@ ifstream file;
 
 char getChar(){
     if(file.is_open())
-        if (!file.eof()) //eof check mikone ke be akhare file residim ya na
-        {
-            file >> c ;
-            return c;
+        if(file.get(c)){
+            return c ;
         }
-    
+        
     return '\0';
     
 }
@@ -23,21 +21,140 @@ void ungetchar(){
     file.seekg(-1,ios::cur);
 }
 
-int main () {
-    file.open ("sample.txt");
-    cout << getChar() << endl;
-    cout << getChar() << endl;
-   
+int isOpChar(char ch){
+    if(ch == '[' || ch == ']' || ch == '(' || ch == ')' ||
+                 ch == '=' || ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || 
+                 ch == '<' || ch == '>' || ch == '!' || ch == '|' || ch == '&' || ch == ';')
+                 return 1;
+    return 0;
+}
+
+int next(char expected){
+    if(getChar() == expected) return 1;
     ungetchar();
-    cout << getChar() << endl;
+    return 0;
+}
+
+string checkOp(char ch){
+    string a = string(1,ch);
+    switch(ch){
+        case '!': next('=') == 1 ? a = "!=" : a = "!"; break;
+        case '=': next('=') == 1 ? a = "==" : a = "="; break;
+        case '>': next('=') == 1 ? a = ">=" : a = ">"; break;
+        case '<': next('=') == 1 ? a = "<=" : a = "<"; break;
+        case '|': next('|') == 1 ? a = "||" : a = "|"; break;
+        case '&': next('&') == 1 ? a = "&&" : a = "&"; break;
+    }
+    return a;
+}
+
+int invalidChar(char ch){
+    return 0;
+}
+
+string getString(string answer, char ch){
+    if(isdigit(ch)){
+        return "error";
+    }
+    // while(ch == ' ' || ch == '\n'){
+    //     ch = getChar();
+    // }
+    //cout << ch << endl;
+    while (! file.eof() && ch != '\0' && ! isOpChar(ch) && ch != ' ' && ch != '\n' && ch != '#' && ! invalidChar(ch) )
+    {
+            answer += ch;
+            ch = getChar();
+            //cout << ch << endl;
+    }
+    if (isOpChar(ch) || ch == '#' || ch == ' ')
+    {
+        ungetchar();
+    }
+    return answer;
+}
+
+void skipComment(){
+    // char ch = getChar();
+    // while (ch != '\n'){
+    //     //cout << "fasele\n";
+    //     ch = getChar();
+    //     //cout << "ch  " << ch << endl;
+    // }
+    //cout << "skip ssss      ............\n";
+    string temp;
+    ungetchar();
+    getline(file, temp);
+    //cout << "comment: " << temp;
+    //cout << file.seekg(0);
+}
+
+char skipSpace(char ch){
+    while (!file.eof() && (ch == ' ' || ch == '\n' || ch == '\t')){
+        //cout << "fasele\n";
+        ch = getChar();
+        //cout << "ch  " << ch << endl;
+    }
+    return ch;
+}
+
+string nextToken(){
+    string answer;
+    char ch ;
+    ch = getChar();
+    if (file.eof()){
+        return "";
+    }
+    if (ch == '#'){
+        cout << "oooooooooooooooo";
+        skipComment();
+        cout << "aaaaaaaaaaaaaaaaaaaa";
+        ch = getChar();
+    }
+
+    ch = skipSpace(ch);
+    cout << "ch:" << ch;
+    //////////////////////////////////
+    if(isOpChar(ch)){
+        //cout << "operation";
+        return checkOp(ch);
+    }
+    else {
+        //cout << "string ..  \n";
+        answer = getString(answer, ch);
+        if (answer == " " || answer == "\n"){
+            nextToken();
+        }
+        return answer;
+    }
     
 
-    // while (ch1  != 0) {
-    //     if (ch1 == ' ')
+}
+
+int main () {
+    file.open ("sample.txt");
+    char ch1 ;
+    string line;
+    // cout << ch1 << endl;
+    // ch1 = getChar();
+    // cout << ch1 << endl;
+    // ch1 = getChar();
+    // cout << ch1 << endl;
+    // ungetchar();
+
+    //int i = 0;
+    while(! file.eof() ){
+        //i++;
+        //cout << "next1111111..... \n";
+        line = nextToken();
+        cout << line << '\n';
+        // cout << "next..... \n";
+    }
+    //cout << "i: " << i << '\n';
+    // while (! file.eof()) {
+    //     if (ch1 == '#')
     //     {
-    //         cout << "return .........\n";
+    //         cout << "comment .........\n";
     //     }
-        
     //     cout << ch1 << '\n';
     //     ch1 = getChar();
     // }
--- tokenizer.cpp
+++ tokenizer.cpp
@@ -8,17 +8,113 @@ ifstream file;
 
 char getChar(){
     if(file.is_open())
-        if (!file.eof()) //eof check mikone ke be akhare file residim ya na
-        {
-            file >> c ;
-            return c;
+        if(file.get(c)){
+            return c ;
         }
-    
+        
     return '\0';
     
 }
 
 void ungetchar(){
     file.seekg(-1,ios::cur);
+}
+
+int isOpChar(char ch){
+    if(ch == '[' || ch == ']' || ch == '(' || ch == ')' ||
+                 ch == '=' || ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || 
+                 ch == '<' || ch == '>' || ch == '!' || ch == '|' || ch == '&' || ch == ';')
+                 return 1;
+    return 0;
+}
+
+int next(char expected){
+    if(getChar() == expected) return 1;
+    ungetchar();
+    return 0;
+}
+
+string checkOp(char ch){
+    string a = string(1,ch);
+    switch(ch){
+        case '!': next('=') == 1 ? a = "!=" : a = "!"; break;
+        case '=': next('=') == 1 ? a = "==" : a = "="; break;
+        case '>': next('=') == 1 ? a = ">=" : a = ">"; break;
+        case '<': next('=') == 1 ? a = "<=" : a = "<"; break;
+        case '|': next('|') == 1 ? a = "||" : a = "|"; break;
+        case '&': next('&') == 1 ? a = "&&" : a = "&"; break;
+    }
+    return a;
+}
+
+int invalidChar(char ch){ //todo
+    return 0;
+}
+
+string getString(string answer, char ch){
+    //todo check string is valid or not
+    while (! file.eof() && ch != '\0' && ! isOpChar(ch) && ch != ' ' && ch != '\n' && ch != '#' && ! invalidChar(ch) )
+    {
+            answer += ch;
+            ch = getChar();
+    }
+    if (isOpChar(ch) || ch == '#' || ch == ' ')
+    {
+        ungetchar();
+    }
+    return answer;
+}
+
+void skipComment(){
+    string temp;
+    ungetchar();
+    getline(file, temp);
+}
+
+char skipSpace(char ch){
+    while (!file.eof() && (ch == ' ' || ch == '\n' || ch == '\r' )){
+        ch = getChar();
+    }
+    return ch;
+}
+
+string nextToken(){
+    string answer;
+    char ch ;
+    ch = getChar();
+    if (file.eof()){
+        return "";
+    }
+    if (ch == '#'){
+        skipComment();
+        ch = getChar();
+    }
+
+    ch = skipSpace(ch);
+    //////////////////////////////////
+    if(isOpChar(ch)){
+        return checkOp(ch);
+    }
+    else {
+        answer = getString(answer, ch);
+        if (answer == " " || answer == "\n"){
+            nextToken();
+        }
+        return answer;
+    }
+    
+
+}
+
+int main () {
+    file.open ("sample.txt");
+    char ch1 ;
+    string line;
+    while(! file.eof() ){
+        line = nextToken();
+        cout << line << '\n';
+    }
+
+  return 0;
 }
 
