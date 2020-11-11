98820c204e640ea6511a8a55d16dedfc033077d3
ZahraReyhanian <reyhanyanz@gmail.com>
2020-11-11 17:18:01 +0330

    create RD_ex.cpp file

---
+++ RD_ex.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void startgrammer(){
+
+}
+
+void anything(){
+
+}
+
+void anything1(){
+
+}
+
+void word(){
+
+}
+
+void number(){
+
+}
+
+void anyword(){
+
+}
+
+void signs(){
+
+}
+
+int main () {
+    return 0;
+}
+
